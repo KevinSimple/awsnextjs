9b1f1bd2732d800829f30cb82dc72e2fd039d076
wangqx@shinetechsoftware.com <wangqx@shinetechsoftware.com>
2020-04-12 14:57:40 +1200

    update news

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,29 @@ function Index(props) {
 }
 
 export async function getServerSideProps () {
-  const url = 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=90f0ceb763e84b54a58f8eebf0ef3436';
+  const Sentry = require('@sentry/node');
+  Sentry.init({ dsn: 'https://a48b0dacfe254d70900616e757730fc4@o370374.ingest.sentry.io/5179766' });
 
-  const res = await fetch(url);
-   const json = await res.json();
-  return {
-    props:json
-  };
+
+  try {
+    const url = 'http://newsapi.org/v2/top-headlines?' + 'country=us&' + 'apiKey=90f0ceb763e84b54a58f8eebf0ef3436';
+
+    const res = await fetch(url);
+    const json = await res.json();
+
+    Sentry.captureMessage(`ssr rendered at ${new Date()}`);
+    return {
+      props:json
+    };
+  }
+  catch(e){
+    Sentry.captureMessage(`ssr ERROR at ${new Date()}`);
+    Sentry.captureException(e);
+    return {
+      props:{}
+    };
+  }
+
 }
 
 export default Index
